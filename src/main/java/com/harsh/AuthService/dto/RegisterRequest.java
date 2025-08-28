9e2acbab36131e6fdf97b3debd28406033f7e5ad
Harsh Srivastava <srivastavah240@gmail.com>
2025-08-28 23:21:00 +0530

    (Feat):Created registered request dto

---
+++ src/main/java/com/harsh/AuthService/dto/RegisterRequest.java
@@ -0,0 +1,35 @@
+package com.harsh.AuthService.dto;
+
+import javax.management.relation.Role;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+//  DTO=Data Transfer Object
+//  It’s a plain Java class that carries data between processes (like frontend → backend, or one service → another).
+/**
+ * Input validation guards your service early.
+ * If role is null, we’ll default to ROLE_USER in the service.
+ */
+// Only this register request type of data  will be sent to service layer
+//Basically it provides Type Safety.
+
+ @Data
+public class RegisterRequest {
+
+     @NotBlank(message = "Username is required")
+      @Size(min = 3, max = 64, message = "Username must be 3-64 characters")
+     private  String username;
+     @Email(message = "Email Shoulb be valid")
+     @NotBlank(message = "Email is required")
+      @Size(max = 254, message = "Email too long")
+    private String email;
+      @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 72, message = "Password must be 6-72 characters")
+    private String password;
+    private Role role;
+
+
+}

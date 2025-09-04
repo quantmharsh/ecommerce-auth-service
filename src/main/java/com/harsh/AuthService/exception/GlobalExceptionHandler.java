ddc1586c33d7c98a3a40d7fd6611fdf374ed8e56
Harsh Srivastava <srivastavah240@gmail.com>
2025-09-04 23:41:57 +0530

    (Feat):Added ExceptionHandler for  usernotfound exception

--- src/main/java/com/harsh/AuthService/exception/GlobalExceptionHandler.java
+++ src/main/java/com/harsh/AuthService/exception/GlobalExceptionHandler.java
@@ -1,25 +1,55 @@
 package com.harsh.AuthService.exception;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+
+//Since using for microservices and Having RESTAPI's its best(@ControllerAdvice+ @ResponseBody)
+//perfect for json response 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     //Handle DTO validation errors(from @Valid)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String , String>> handleValidationErrors(MethodArgumentNotValidException ex)
+    public ResponseEntity<ApiError> handleValidationErrors(MethodArgumentNotValidException ex)
     {
         Map<String , String> errors= new HashMap<>();
         ex.getBindingResult().getFieldErrors().forEach(error->errors.put(error.getField() ,error.getDefaultMessage()));
-        //return 404 with errors map
-        return ResponseEntity.badRequest().body(errors);
+        
+       ApiError apiError = new ApiError(
+                Instant.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation Error",
+                "Invalid request parameters",
+               errors
+        );
+        return ResponseEntity.badRequest().body(apiError);
 
+    }
+
+    //User Not found
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ApiError>   handleUserNotFound(UserNotFoundException ex)
+    {
+        ApiError  apiError= new ApiError(
+                Instant.now(),
+                HttpStatus.NOT_FOUND.value(),
+                "User Not Found",
+                ex.getMessage(),
+                null);
+
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
     }
 
 }

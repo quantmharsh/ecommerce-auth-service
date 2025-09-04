69cb55b644df60d2bc6407c820c794fda9248a35
Harsh Srivastava <srivastavah240@gmail.com>
2025-09-04 23:11:20 +0530

    (Feat):Handle DT0 Validation error @Valid

--- src/main/java/com/harsh/AuthService/exception/GlobalExceptionHandler.java
+++ src/main/java/com/harsh/AuthService/exception/GlobalExceptionHandler.java
@@ -1,8 +1,25 @@
 package com.harsh.AuthService.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    //Handle DTO validation errors(from @Valid)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String , String>> handleValidationErrors(MethodArgumentNotValidException ex)
+    {
+        Map<String , String> errors= new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error->errors.put(error.getField() ,error.getDefaultMessage()));
+        //return 404 with errors map
+        return ResponseEntity.badRequest().body(errors);
+
+    }
 
 }
